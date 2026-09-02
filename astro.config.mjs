c037545f8ee833c6627c6a6473cd96981dbf33b5
elsi <elsamed.alija@gmail.com>
2026-09-02 23:29:31 +0200

    Add Instagram social link

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Dokumente',
+			social: [{ icon: 'instagram', label: 'Instagram', href: 'https://instagram.com/elsi_alija' }],
 			sidebar: [
 				{
 					label: 'Guides',
