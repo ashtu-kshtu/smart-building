4afd6cf91de5a5bb61f717ee0fa342a262b5e9ec
elsi <elsamed.alija@gmail.com>
2026-09-03 20:12:16 +0200

    Website Name

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
     integrations: [
         starlight({
-            title: 'Dokumente',
+            title: 'Smart-building',
             social: [{ icon: 'instagram', label: 'Instagram', href: 'https://instagram.com/elsi_alija' }],
             sidebar: [
                 {
